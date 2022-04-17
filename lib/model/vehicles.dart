efb86313cd5ec3fa33cc59851443fbe8dcfa8275
Heshan <heshan.imedha@gmail.com>
2022-04-17 09:32:57 +0530

    second commit

--- lib/model/vehicles.dart
+++ lib/model/vehicles.dart
@@ -1,42 +1,64 @@
-class Album {
+import 'dart:convert';
+import '../main.dart';
+import 'dart:convert';
+
+Vehicle vehicleFromJson(String str) => Vehicle.fromJson(json.decode(str));
+
+String vehicleToJson(Vehicle data) => json.encode(data.toJson());
+
+class Vehicle {
+  Vehicle({
+    required this.count,
+    required this.message,
+    required this.searchCriteria,
+    required this.results,
+  });
+
   int count;
   String message;
   String searchCriteria;
-  List<Results> results;
+  List<Result> results;
 
-  Album(
-      {required this.count,
-      required this.message,
-      required this.searchCriteria,
-      required this.results});
+  factory Vehicle.fromJson(Map<String, dynamic> json) => Vehicle(
+        count: json["Count"],
+        message: json["Message"],
+        searchCriteria: json["SearchCriteria"],
+        results:
+            List<Result>.from(json["Results"].map((x) => Result.fromJson(x))),
+      );
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      count: json['Count'],
-      message: json['Message'],
-      searchCriteria: json['SearchCriteria'],
-      results: (json['Results'] as List).cast<Results>(),
-    );
-  }
+  Map<String, dynamic> toJson() => {
+        "Count": count,
+        "Message": message,
+        "SearchCriteria": searchCriteria,
+        "Results": List<dynamic>.from(results.map((x) => x.toJson())),
+      };
 }
 
-class Results {
-  int makeID;
-  String makeName;
-  int modelID;
-  String modelName;
-  Results(
-      {required this.makeID,
-      required this.makeName,
-      required this.modelID,
-      required this.modelName});
+class Result {
+  Result({
+    required this.makeId,
+    required this.makeName,
+    required this.modelId,
+    required this.modelName,
+  });
 
-  factory Results.fromJson(Map<String, dynamic> json) {
-    return Results(
-      makeID: json['Make_ID'],
-      makeName: json['Make_Name'],
-      modelID: json['Model_ID'],
-      modelName: json['Model_Name'],
-    );
-  }
+  int makeId;
+  String makeName;
+  int modelId;
+  String modelName;
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        makeId: json["Make_ID"],
+        makeName: json["Make_Name"],
+        modelId: json["Model_ID"],
+        modelName: json["Model_Name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "Make_ID": makeId,
+        "Make_Name": makeName,
+        "Model_ID": modelId,
+        "Model_Name": modelName,
+      };
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,8 +2,20 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../model/vehicles.dart';
-import '../services/services.dart';
 import '../widgets/vehicle_list.dart';
+
+Future<List<Result>> getVehicles() async {
+  final response = await http.get(Uri.parse(
+      'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json#'));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body).map<Result>((data) {
+      return Result.fromJson(data);
+    }).toList();
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -16,32 +28,28 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Vehicle List'),
-      ),
-      body: FutureBuilder(
-          future: NetworkService.fetchAlbum(),
-          builder: (context, AsyncSnapshot<Results> snapshot) {
-            if (snapshot.hasData) {
-              List<Results>? results = snapshot.data as List<Results>?;
+        appBar: AppBar(
+          title: Text('Vehicle List'),
+        ),
+        body: FutureBuilder<List<Result>>(
+            future: getVehicles(),
+            builder: (context, snapshot) {
+              if (snapshot == ConnectionState.done) {
+                List<Result>? results = snapshot.data;
+                return Container(
+                  child: results != null
+                      ? VehicleList(results: results)
+                      : const Text('Noooooooooo !'),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('${snapshot.error}'),
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-              return Container(
-                child: results != null
-                    ? VehicleList(
-                        vehicles: results,
-                      )
-                    : const Text('Noooooooooo !'),
-              );
-            } else if (snapshot.hasError) {
-              print(snapshot.error);
-              return Center(
-                child: Text('${snapshot.error}'),
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }),
-    );
+              return const CircularProgressIndicator();
+            }));
   }
 }
